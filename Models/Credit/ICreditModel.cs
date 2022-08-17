cfb90fee63610e9dd9156d4d1a2886cfc2c31c7a
tehtko <moderndsnsam@gmail.com>
2022-08-17 16:39:27 -0400

    Populate ICreditModel

---
+++ Models/Credit/ICreditModel.cs
@@ -0,0 +1,15 @@
+namespace GDBank.Models
+{
+    public interface ICreditModel
+    {
+        public int Id { get; set; }
+        public int MonthLimit { get; set; }
+        public string AccountName { get; set; }
+        public string CardHolder { get; set; }
+        public float? CashBack { get; set; }
+        public float MonthlyFee { get; set; }
+        public float InterestRate { get; set; }
+
+        public int AccountId { get; set; }
+    }
+}
