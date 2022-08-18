eecd75b893fa1fd42fc500ee24b4724037418d94
tehtko <moderndsnsam@gmail.com>
2022-08-17 20:26:03 -0400

    Declare properties non-nullable

--- Models/AccountCreationModel.cs
+++ Models/AccountCreationModel.cs
@@ -2,7 +2,7 @@ namespace GDBank.Models;
 
 public class AccountCreationModel
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? ConfirmPassword { get; set; }
 }
