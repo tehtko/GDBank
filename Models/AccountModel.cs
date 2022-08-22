145ad7eb9734fcc9350ee6109827538dc8357e8d
tehtko <moderndsnsam@gmail.com>
2022-08-21 20:35:53 -0400

    Update account model

--- Models/AccountModel.cs
+++ Models/AccountModel.cs
@@ -3,12 +3,12 @@ namespace GDBank.Models;
 public class AccountModel
 {
     public int Id { get; set; }
-    public string Username { get; set; }
+    public string Email { get; set; }
     public string Password { get; set; }
 
-    public AccountModel(string username, string password)
+    public AccountModel(string email, string password)
     {
-        Username = username;
+        Email = email;
         Password = password;
     }
 }
