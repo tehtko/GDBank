19cba29ee4a6c2f408b5c8918108def57945d5f4
tehtko <moderndsnsam@gmail.com>
2022-08-21 20:36:42 -0400

    Change Id type

--- Models/AccountModel.cs
+++ Models/AccountModel.cs
@@ -2,7 +2,7 @@ namespace GDBank.Models;
 
 public class AccountModel
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
 
