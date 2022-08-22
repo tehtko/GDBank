d1bf202e32302f60dc1b7bd5b9f1e9b4d8a03d5c
tehtko <moderndsnsam@gmail.com>
2022-08-21 20:38:50 -0400

    Remove constructor

--- Models/AccountModel.cs
+++ Models/AccountModel.cs
@@ -5,10 +5,4 @@ public class AccountModel
     public Guid Id { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-
-    public AccountModel(string email, string password)
-    {
-        Email = email;
-        Password = password;
-    }
 }
