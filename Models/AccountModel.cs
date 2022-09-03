617977f7dce230f082b63d68ea35cfb2e6cf8b0e
tehtko <moderndsnsam@gmail.com>
2022-09-03 18:23:40 -0400

    Change Account id  data type to int

--- Models/AccountModel.cs
+++ Models/AccountModel.cs
@@ -2,7 +2,7 @@ namespace GDBank.Models;
 
 public class AccountModel
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
 }
