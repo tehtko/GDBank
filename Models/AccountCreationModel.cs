8c21d4f4d7fa9e39b910e81b924c3e35710f162d
tehtko <moderndsnsam@gmail.com>
2022-09-03 18:43:12 -0400

    Add data annotations

--- Models/AccountCreationModel.cs
+++ Models/AccountCreationModel.cs
@@ -4,17 +4,17 @@ namespace GDBank.Models;
 
 public class AccountCreationModel
 {
-    [Required(ErrorMessage = "Please provide your full name")]
+    [Required(ErrorMessage = "Please provide your full name!")]
     public string? FullName { get; set; }
 
-    [Required(ErrorMessage = "Please provide a valid email")]
+    [Required(ErrorMessage = "Please provide a valid email!")]
     public string? Email { get; set; }
 
-    [Required(ErrorMessage = "Please provide a valid password")]
+    [Required(ErrorMessage = "Please provide a valid password!")]
     [DataType(DataType.Password)]
     public string? Password { get; set; }
 
     [DataType(DataType.Password)]
-    [Compare("Password", ErrorMessage = "Passwords do not match")]
+    [Compare("Password", ErrorMessage = "Passwords do not match!")]
     public string? ConfirmPassword { get; set; }
 }
