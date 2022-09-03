aa62b7f07a5d4c54d190e9ac7f320b81e3265c8b
tehtko <moderndsnsam@gmail.com>
2022-09-03 18:35:33 -0400

    Remove constructor

--- Models/AccountCreationModel.cs
+++ Models/AccountCreationModel.cs
@@ -5,10 +5,4 @@ public class AccountCreationModel
     public string? Email { get; set; }
     public string? Password { get; set; }
     public string? ConfirmPassword { get; set; }
-
-    public AccountCreationModel(string email, string password)
-    {
-        Email = email;
-        Password = password;
-    }
 }
