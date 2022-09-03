8b807c2158ad856f91ab1bd5100fe52f83a18c76
tehtko <moderndsnsam@gmail.com>
2022-09-03 18:36:30 -0400

    Add FullName property

--- Models/AccountModel.cs
+++ Models/AccountModel.cs
@@ -3,6 +3,7 @@ namespace GDBank.Models;
 public class AccountModel
 {
     public int Id { get; set; }
+    public string FullName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
 }
