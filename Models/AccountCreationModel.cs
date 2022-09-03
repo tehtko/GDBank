0ff9e4f3cbc7d75aaf0a1745c62cd4d2ae1979a3
tehtko <moderndsnsam@gmail.com>
2022-09-03 18:42:15 -0400

    Add ValidationScripts

--- Models/AccountCreationModel.cs
+++ Models/AccountCreationModel.cs
@@ -1,8 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GDBank.Models;
 
 public class AccountCreationModel
 {
+    [Required(ErrorMessage = "Please provide your full name")]
+    public string? FullName { get; set; }
+
+    [Required(ErrorMessage = "Please provide a valid email")]
     public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Please provide a valid password")]
+    [DataType(DataType.Password)]
     public string? Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Passwords do not match")]
     public string? ConfirmPassword { get; set; }
 }
