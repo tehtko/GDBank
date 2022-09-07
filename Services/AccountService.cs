faa0ab60279af323b3bc99779eddc2f06a8d30c1
tehtko <moderndsnsam@gmail.com>
2022-09-07 18:01:02 -0400

    Create Login and Signup service methods

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -1,6 +1,70 @@
+using GDBank.Data;
+using GDBank.Models;
+using System.Data.SqlClient;
+
 namespace GDBank.Services;
 
 public class AccountService
 {
+    public bool CreateUser(AccountModel user)
+    {
+        try
+        {
+            GDContext context = new();
+            SqlDataAdapter adapter = new();
 
+            string password = Hash.HashPassword(user.Password);
+
+            context.connection.Open();
+
+            SqlCommand command = new()
+            {
+                Connection = context.connection,
+                CommandText = $"INSERT INTO GDUsers (full_name, email, password) VALUES ('{user.FullName}', '{user.Email}', '{password}')"
+            };
+
+            adapter.InsertCommand = command;
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            command.Dispose();
+            context.connection.Close();
+
+            return true;
+        }
+        catch (SqlException)
+        {
+            return false;
+        }
+    }
+
+    public bool ValidateLogin(AccountModel user)
+    {
+        try
+        {
+            GDContext context = new();
+            SqlDataReader dataReader;
+
+            string password = Hash.HashPassword(user.Password);
+
+            context.connection.Open();
+
+            SqlCommand command = new($"SELECT username, password FROM FlashTyperUsers WHERE username = '{user.Email}' AND password = '{password}';", context.connection);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                if (dataReader.GetString(1) == user.Email && dataReader.GetString(2) == password)
+                    return true;
+                else
+                    return false;
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
 }
