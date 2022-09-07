c7a7ccbb6cf542399b8c78e202ecf344428b398b
tehtko <moderndsnsam@gmail.com>
2022-09-07 17:49:26 -0400

    Add IActionResults

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,6 +48,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Account()
+    {
+        return View();
+    }
+
+    public IActionResult Store()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
