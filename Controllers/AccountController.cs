c5cb469106408955561b76ca7b93b10d2bccfb0d
tehtko <moderndsnsam@gmail.com>
2022-09-08 16:57:51 -0400

    Use Action redirects

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -9,25 +9,34 @@ public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
     private AccountService accountService = new();
-
     public AccountController(ILogger<AccountController> logger)
     {
         _logger = logger;
     }
 
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Profile()
+    {
+        return View("Account");
+    }
+
     public IActionResult Login(AccountModel user)
     {
         if (accountService.ValidateLogin(user) is false)
-            return View("Index");
+            return RedirectToAction("Index");
         else
-            return View("Account");
+            return RedirectToAction("Profile");
     }
 
     public IActionResult Signup(AccountCreationModel user)
     {
         if (accountService.CreateUser(user) is false)
-            return View("Index");
+            return RedirectToAction("Index");
         else
-            return View("Account");
+            return RedirectToAction("Profile");
     }
 }
