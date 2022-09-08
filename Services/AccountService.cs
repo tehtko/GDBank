32071f4585c0d588e7f828580b05a1b641c508b5
tehtko <moderndsnsam@gmail.com>
2022-09-08 16:58:46 -0400

    Fix dataReader index out of bounds

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -51,19 +51,19 @@ public class AccountService
 
             context.connection.Open();
 
-            SqlCommand command = new($"SELECT email, password FROM FlashTyperUsers WHERE email = '{user.Email}' AND password = '{password}';", context.connection);
+            SqlCommand command = new($"SELECT email, password FROM GDUsers WHERE email = '{user.Email}' AND password = '{password}';", context.connection);
 
             dataReader = command.ExecuteReader();
 
             while (dataReader.Read())
             {
-                if (dataReader.GetString(1) == user.Email && dataReader.GetString(2) == password)
+                if (dataReader.GetString(0) == user.Email && dataReader.GetString(1) == password)
                     return true;
                 else
-                    return false;
+                    break;
             }
 
-            return true;
+            return false;
         }
         catch (Exception)
         {
