7d9e75a7e0e22a56feb7dfc8d26b16da2832d21b
tehtko <moderndsnsam@gmail.com>
2022-09-10 16:06:07 -0400

    Create first card models

---
+++ Models/Credit/CashbackCreditModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GDBank.Models.Credit
+{
+    public class CashbackCreditModel : ICreditModel
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public float Balance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int MonthLimit { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string AccountName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CardHolder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public float? CashBack { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public float MonthlyFee { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public float InterestRate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int AccountId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Models/Debit/CashbackDebitModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GDBank.Models.Debit
+{
+    public class CashbackDebitModel : IDebitModel
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public float Balance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int MonthLimit { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string AccountName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CardHolder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public float? CashBack { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public float? MonthlyFee { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool OverdraftProtection { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int AccountId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
