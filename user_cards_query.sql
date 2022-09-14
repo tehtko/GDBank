dcac6f97287593a7f7746e560ca1c7e1b35d04d9
tehtko <moderndsnsam@gmail.com>
2022-09-14 19:59:49 -0400

    Card table creation query

---
+++ user_cards_query.sql
@@ -0,0 +1,13 @@
+CREATE TABLE GDUsers_Cards  
+(
+    id INT PRIMARY KEY,
+    balance FLOAT NOT NULL,
+    month_limit INT NOT NULL,
+    account_name VARCHAR(MAX) NOT NULL,
+    card_holder VARCHAR(MAX) NOT NULL,
+    cashback FLOAT NOT NULL,
+    monthly_fee FLOAT NOT NULL,
+    interest_rate FLOAT NOT NULL,
+    overdraft_protection INT NOT NULL,
+    account_id INT NOT NULL
+);
