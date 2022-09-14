b742d6339fb43e7e1f16f186d895d101bc4f09e0
tehtko <moderndsnsam@gmail.com>
2022-09-13 23:27:10 -0400

    Add error messages for failed login/signup

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -31,8 +31,11 @@ public class AccountController : Controller
     {
         // If unable to create the account redirect the user to the homepage
         if (accountService.CreateUser(user) is false)
-            return RedirectToAction("Index");
-
+        {
+            ViewBag.SignupError = "This e-mail is taken";
+            return View();
+        }
+            
         // Log and set session state if signup was successful
         Log.Information("Account {0} created at {1}", user.Email, DateTime.UtcNow);
         HttpContext.Session.SetString("UserSession", JsonConvert.SerializeObject(user));
@@ -43,8 +46,11 @@ public class AccountController : Controller
     {
         // If unable to login redirect the user to the homepage
         if (accountService.ValidateLogin(user) is false)
-            return RedirectToAction("Index");
-
+        {
+            ViewBag.SignupError = "Username or password is incorrect";
+            return View();
+        }
+            
         // Log and set session state if login was successful
         Log.Information("User {0} logged in at {1}", user.Email, DateTime.UtcNow);
         HttpContext.Session.SetString("UserSession", JsonConvert.SerializeObject(user));
