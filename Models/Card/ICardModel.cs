bcb3f1e30e0f9ba26e1c5a7c848b0b2c7b577ff3
tehtko <moderndsnsam@gmail.com>
2022-09-14 20:16:42 -0400

    Rename folder

---
+++ Models/Card/ICardModel.cs
@@ -0,0 +1,17 @@
+namespace GDBank.Models;
+
+public interface ICardModel
+{
+    public int Id { get; set; }
+    public int CardType { get; set; } // 0 = debit, 1 = credit
+    public float Balance { get; set; }
+    public int MonthLimit { get; set; } // monthly spending power
+    public string AccountType { get; set; } // eg. cashback, business, travel
+    public string CardHolder { get; set; }
+    public float CashBack { get; set; }
+    public float MonthlyFee { get; set; } // none for students
+    public float InterestRate { get; set; } // credit only
+    public int OverdraftProtection { get; set; } // debit only
+
+    public int AccountId { get; set; }
+}
