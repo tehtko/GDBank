0f1b9801026476e8a9f785a5808d717428c3be32
tehtko <moderndsnsam@gmail.com>
2022-09-14 20:10:38 -0400

    Rename AccountName

--- Models/Credit/ICardModel.cs
+++ Models/Credit/ICardModel.cs
@@ -6,7 +6,7 @@ public interface ICardModel
     public int CardType { get; set; } // 0 = debit, 1 = credit
     public float Balance { get; set; }
     public int MonthLimit { get; set; } // monthly spending power
-    public string AccountName { get; set; } // eg. cashback, business, travel
+    public string AccountType { get; set; } // eg. cashback, business, travel
     public string CardHolder { get; set; }
     public float CashBack { get; set; }
     public float MonthlyFee { get; set; } // none for students
