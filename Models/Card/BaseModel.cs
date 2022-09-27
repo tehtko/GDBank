45970343687ce3bd202a400f7d642686527672a5
tehtko <moderndsnsam@gmail.com>
2022-09-26 22:26:33 -0400

    Create BaseModel for enumeration

---
+++ Models/Card/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿namespace GDBank.Models.Card
+{
+    public class BaseModel : ICardModel
+    {
+        public int Id { get; set; }
+        public string CardType { get; set; }
+        public float Balance { get; set; }
+        public int MonthLimit { get; set; }
+        public string AccountType { get; set; }
+        public string CardHolder { get; set; }
+        public float CashBack { get; set; }
+        public float MonthlyFee { get; set; }
+        public float InterestRate { get; set; }
+        public int OverdraftProtection { get; set; }
+        public int AccountId { get; set; }
+    }
+}
